ce83291c51a687763e1e2e5342512d0f882ffe3f
BosEriko <bos.eriko@gmail.com>
2020-07-22 09:50:05 +0800

    add editmode to the project

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Editmode, Chunk } from "editmode-react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Editmode projectId="prj_h3Gk3gFVMXbl">
+        <Chunk identifier="cnk_2177d77492a2dead1585" />
+        <StatusBar style="auto" />
+      </Editmode>
     </View>
   );
 }
