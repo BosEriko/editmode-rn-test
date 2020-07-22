56279ed78cec3d73606c01af586436a7d9fd3d66
BosEriko <bos.eriko@gmail.com>
2020-07-22 09:50:50 +0800

    remove text from the imports

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Editmode, Chunk } from "editmode-react";
 
 export default function App() {
